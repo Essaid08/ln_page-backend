d4f108a5090dcc87567c277a377b71fb51620219
said <assaidbouhenni3@gmail.com>
2024-12-21 03:37:03 +0100

    add the docker file

--- app.js
+++ app.js
@@ -13,30 +13,30 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+	res.send("Backend is running!");
+});
 
-// defining middlewears 
+// defining middlewears
 app.use("/api/palindrom", palindromRouter);
 app.use("/api/perfect_number", perfectNumberRouter);
-app.use("/api/fibonacci" , fibonacciRouter)
-
+app.use("/api/fibonacci", fibonacciRouter);
 
 // Express globale error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
+	console.error(err.stack);
 
-    // Determine the response status code
-    const statusCode = err.status || 500;
+	// Determine the response status code
+	const statusCode = err.status || 500;
 
-    // build the simple response object
-    const response = {
-        success: false,
-        message: err.message || "Internal Server Error",
-    };
-    // Send the  error response
-    res.status(statusCode).json(response);
+	// build the simple response object
+	const response = {
+		success: false,
+		message: err.message || "Internal Server Error",
+	};
+	// Send the  error response
+	res.status(statusCode).json(response);
 });
-
-
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
